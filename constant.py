fececc933b687e342cfa34b39d9c50a620681173
Yuri Zhang <57687336+Yuri0203@users.noreply.github.com>
2024-04-16 13:50:35 +0800

    Update constant.py

--- constant.py
+++ constant.py
@@ -36,9 +36,12 @@ var vizElementD = document.getElementById('vizContainerDesktop NV');
 vizElementD.style.display='block';
 </script>
 """,
-                  'tableau_2':
-                  """<div class='tableauPlaceholder' id='viz1713246192427' style='position: relative'><noscript><a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;NI&#47;NIHRResearchAwards&#47;DataQuality&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='NIHRResearchAwards&#47;DataQuality' /><param name='tabs' value='yes' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;NI&#47;NIHRResearchAwards&#47;DataQuality&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1713246192427');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1366px';vizElement.style.height='950px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1366px';vizElement.style.height='950px';} else { vizElement.style.width='100%';vizElement.style.height='3850px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                
-                  </script>
-                  """
+                 'tableau_2': 
+                """<div id="vizContainerDesktop NV" style="width:100%; height:820px;"><iframe frameborder="0" allowtransparency="true" allowfullscreen="true" title="'NIHR Research Award" marginheight="0" marginwidth="0" scrolling="no" style="display: block; min-width: 400px; width:100%; max-width: 1700px; height: 790px; visibility: visible;" src=" https://public.tableau.com/views/NIHRResearchAwards/DataQuality?:language=en-US&:display_count=n&?:embed=y&:showVizHome=n&:device=desktop""></iframe></div> 
+<script type='text/javascript'>          
+var vizElementD = document.getElementById('vizContainerDesktop NV');
+vizElementD.style.display='block';
+</script>
+"""
                  }
 
