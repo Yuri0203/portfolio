4f3d17325f3b80009fb7d9d3616ee4cc34a7954b
Yuri Zhang <57687336+Yuri0203@users.noreply.github.com>
2024-04-16 13:47:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,15 +86,16 @@ st.markdown('<ul>'+achievement_list+'</ul>',unsafe_allow_html=True)
 
 st.subheader('Tableau BI Projects 📊')
 #st.markdown("""<a href={}> access full profile here</a>""".format(info['tableau']),unsafe_allow_html=True)
+
 st.markdown('<h5><u>Supplier Scorecard</h5>',unsafe_allow_html=True)
 st.caption('work sample with Dummy Data')
 with st.expander('read data viz sample below'):
     components.html(embed_component['tableau'],height=800)
 
-#st.markdown('<h5><u>Multi-Service Line BI Analytics</h5>',unsafe_allow_html=True)
-#st.caption('Mockup with Dummy Data')
-#with st.expander('read data viz sample below'):
-#    components.html(embed_component['tableau'],height=800)
+
+st.markdown('<h5><u>NIHR Research Award Analysis</h5>',unsafe_allow_html=True)
+with st.expander('read data viz sample below'):
+    components.html(embed_component['tableau_2'],height=800)
 
 #st.sidebar.caption('Wish to connect?')
 st.sidebar.subheader('Contact')
